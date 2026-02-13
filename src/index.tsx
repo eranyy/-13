dd17005b534dc3717b18573e932ffaa9fd702bbe
eranyy <84619737+eranyy@users.noreply.github.com>
2026-02-13 16:29:46 +0200

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './index.css'; // השורה הזו מחברת את העיצוב לאתר
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
